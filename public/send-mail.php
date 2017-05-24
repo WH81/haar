e70453f8f2cc8a8124a4e9dbb0b6182748edc768
Wayne Hite <waynehite@new-host-2.home>
2017-05-24 11:29:47 -0400

    Headers as array

--- public/send-mail.php
+++ public/send-mail.php
@@ -9,36 +9,45 @@ $year = $_POST['year'];
 $customer = $_POST['customer']; // required
 $comments = $_POST['comments']; // required
 
-$message = '<html><body>';
-$message .= '<img class="logo img-responsive" src="images/logos/logo.svg" alt="Hite Autobody and Restoration">';
-$message .= '<table style="border-color: #666;" cellpadding="5">';
-$message .= "<tr style='background: #e0e0e0;'><td><strong>Name:</strong> </td><td>" . strip_tags($_POST['name']) . "</td></tr>";
-$message .= "<tr><td><strong>Email:</strong> </td><td>" . strip_tags($_POST['email']) . "</td></tr>";
-$message .= "<tr><td><strong>Phone:</strong> </td><td>" . strip_tags($_POST['phone']) . "</td></tr>";
-$message .= "<tr><td><strong>Make:</strong> </td><td>" . strip_tags($_POST['make']) . "</td></tr>";
-$message .= "<tr><td><strong>Model:</strong> </td><td>" . strip_tags($_POST['model']) . "</td></tr>";
-$message .= "<tr><td><strong>Year:</strong> </td><td>" . strip_tags($_POST['year']) . "</td></tr>";
-$message .= "<tr><td><strong>Customer:</strong> </td><td>" . strip_tags($_POST['customer']) . "</td></tr>";
-$message .= "<tr><td><strong>Comments:</strong> </td><td>" . strip_tags($_POST['comments']) . "</td></tr>";
-$message .= "</table>";
-$message .= '</body></html>';
-
 $to = 'example@yahoo.com';
-$subject = "Service inquiry from " . $name . ".";
-$from = 'Name here <example@yahoo.com>';
+$subject = 'Service inquiry from ' . $name . '.';
+$message = '<html><body>';
+$message .= '<img src="http://www.example.com/qa/qa/public/images/logos/logo.svg" alt="example Autobody and Restoration">';
+$message .= '<table style="border-color: #666;" cellpadding="5">';
+$message .= '<tr style="background: #e0e0e0;"><td><strong>Name:</strong></td><td>' . strip_tags($_POST['name']) . '</td></tr>';
+$message .= '<tr><td><strong>Email:</strong></td><td>' . strip_tags($_POST['email']) . '</td></tr>';
+$message .= '<tr><td><strong>Phone:</strong></td><td>' . strip_tags($_POST['phone']) . '</td></tr>';
+$message .= '<tr><td><strong>Make:</strong></td><td>' . strip_tags($_POST['make']) . '</td></tr>';
+$message .= '<tr><td><strong>Model:</strong></td><td>' . strip_tags($_POST['model']) . '</td></tr>';
+$message .= '<tr><td><strong>Year:</strong></td><td>' . strip_tags($_POST['year']) . '</td></tr>';
+$message .= '<tr><td><strong>Customer:</strong></td><td>' . strip_tags($_POST['customer']) . '</td></tr>';
+$message .= '<tr><td><strong>Comments:</strong></td><td>' . strip_tags($_POST['comments']) . '</td></tr>';
+$message .= '</table>';
+$message .= '</body></html>';
+$from = 'example <example@example.com>';
 
-$headers = 'From: {$from}\n'; 
-$headers .= 'Reply-To: {$email}\n'; 
+/*
+$headers = 'From: {$from}'; 
+$headers .= 'Reply-To: {$email}'; 
 $headers .= 'X-Mailer: PHP/' .phpversion(). '\n';		 
-$headers .= 'MIME-Version: 1.0\n';
+$headers .= 'MIME-Version: 1.0';
 $headers .= 'Content-Type: text/html; charset=iso-8859-1\n';	
-		   
-$authorized = '-fexample@yahoo.com';
+*/		
 
-$result = mail($to, $subject, $message, $headers, $authorized);
+// To send HTML mail, the Content-type header must be set
+$headers[] = 'MIME-Version: 1.0';
+$headers[] = 'Content-type: text/html; charset=iso-8859-1';
 
-echo $result ? '<h3>Hello, ' . $name . '.</h3>' . '<h4>Thank you for contacting  Autobody & Restoration!</h4>' . '<h4>We have received your message and will reply as soon as possible.</h4>' : '<h4>Sorry, your mail has not sent. Please try again.</h4>';
+// Additional headers
+$headers[] = 'To: Wayne <example@yahoo.com>';
+$headers[] = 'From: example <example@yahoo.com>';
+   
+// $authorized = '-fexample@yahoo.com';
+// $result = mail($to, $subject, $message, $headers, $authorized);
 
-
-
+ mail($to, $subject, $message, implode("\r\n", $headers));
+ 
+ 
+ 
+echo $result ? '<h3>Hello, ' . $name . '.</h3>' . '<h4>Thank you for contacting example Autobody & Restoration!</h4>' . '<h4>We have received your message and will reply as soon as possible.</h4>' : '<h4>Sorry, your mail has not sent. Please try again.</h4>';
 
