2fb0f156807a5b0d8a6b3454d12929871c99efe5
Wayne Hite <waynehite@new-host.home>
2017-05-26 17:07:24 -0400

    May 26 updates

--- public/send-mail.php
+++ public/send-mail.php
@@ -9,49 +9,41 @@ $year = $_POST['year'];
 $customer = $_POST['customer']; // required
 $comments = $_POST['comments']; // required
 
-$to = 'example@yahoo.com';
+$to = 'Wayne W. Hite <waynehite@yahoo.com>';
 $subject = 'Service inquiry from ' . $name . '.';
-$message = '<html><body>';
-<<<<<<< HEAD
-$message .= '<img src="http://www.example.com/qa/qa/public/images/logos/logo.svg" alt="example Autobody and Restoration">';
-=======
-$message .= '<img class="logo img-responsive" src="images/logos/logo.svg" alt="haar">';
->>>>>>> 158e1e533951130d2589e3af4cc929f49f4bda61
-$message .= '<table style="border-color: #666;" cellpadding="5">';
-$message .= '<tr style="background: #e0e0e0;"><td><strong>Name:</strong></td><td>' . strip_tags($_POST['name']) . '</td></tr>';
-$message .= '<tr><td><strong>Email:</strong></td><td>' . strip_tags($_POST['email']) . '</td></tr>';
-$message .= '<tr><td><strong>Phone:</strong></td><td>' . strip_tags($_POST['phone']) . '</td></tr>';
-$message .= '<tr><td><strong>Make:</strong></td><td>' . strip_tags($_POST['make']) . '</td></tr>';
-$message .= '<tr><td><strong>Model:</strong></td><td>' . strip_tags($_POST['model']) . '</td></tr>';
-$message .= '<tr><td><strong>Year:</strong></td><td>' . strip_tags($_POST['year']) . '</td></tr>';
-$message .= '<tr><td><strong>Customer:</strong></td><td>' . strip_tags($_POST['customer']) . '</td></tr>';
-$message .= '<tr><td><strong>Comments:</strong></td><td>' . strip_tags($_POST['comments']) . '</td></tr>';
-$message .= '</table>';
-$message .= '</body></html>';
-$from = 'example <example@example.com>';
 
-/*
-$headers = 'From: {$from}'; 
-$headers .= 'Reply-To: {$email}'; 
-$headers .= 'X-Mailer: PHP/' .phpversion(). '\n';		 
-$headers .= 'MIME-Version: 1.0';
-$headers .= 'Content-Type: text/html; charset=iso-8859-1\n';	
-*/		
+
+$message = '<html><body style="background-color:rgba(0,0,0,0.05);width:100%;padding:5px;">';
+$message .= '<div style="margin:0 auto;text-align:center;background-color:rgba(224,224,224,0.05);">';
+$message .= '<img style="text-align:center;width:100%;max-width:375px;height:auto;margin:0 auto;" src="http://www.hiteautobodyandrestoration.com/qa/qa/public/images/logos/hite-footer-logo.svg" alt="Hite Autobody and Restoration">';
+$message .= '<h2 style="text-align:center;background:rgba(0,0,0,0.05);border-bottom:1px solid #e0e0e0;padding:5px;font-size:110%;">Customer Form Data</h2>';
+$message .= '<table style="text-align:left;margin:0 auto;width:100%;max-width:575px;">';
+$message .= '<tr style="background:rgba(0,0,0,0.02);line-height:175%;"><td style="border:1px solid #e0e0e0;padding:5px;"><strong>Name:</strong></td><td style="border:1px solid #e0e0e0;padding:5px;">' . strip_tags($_POST['name']) . '</td></tr>';
+$message .= '<tr style="background:rgba(0,0,0,0.05);line-height:175%;"><td style="border:1px solid #e0e0e0;padding:5px;"><strong>Email:</strong></td><td style="border:1px solid #e0e0e0;padding:5px;">' . strip_tags($_POST['email']) . '</td></tr>';
+$message .= '<tr style="background:rgba(0,0,0,0.02);line-height:175%;"><td style="border:1px solid #e0e0e0;padding:5px;"><strong>Phone:</strong></td><td style="border:1px solid #e0e0e0;padding:5px;">' . strip_tags($_POST['phone']) . '</td></tr>';
+$message .= '<tr style="background:rgba(0,0,0,0.05);line-height:175%;"><td style="border:1px solid #e0e0e0;padding:5px;"><strong>Make:</strong></td><td style="border:1px solid #e0e0e0;padding:5px;">' . strip_tags($_POST['make']) . '</td></tr>';
+$message .= '<tr style="background:rgba(0,0,0,0.02);line-height:175%;"><td style="border:1px solid #e0e0e0;padding:5px;"><strong>Model:</strong></td><td style="border:1px solid #e0e0e0;padding:5px;">' . strip_tags($_POST['model']) . '</td></tr>';
+$message .= '<tr style="background:rgba(0,0,0,0.05);line-height:175%;"><td style="border:1px solid #e0e0e0;padding:5px;"><strong>Year:</strong></td><td style="border:1px solid #e0e0e0;padding:5px;">' . strip_tags($_POST['year']) . '</td></tr>';
+$message .= '<tr style="background:rgba(0,0,0,0.02);line-height:175%;"><td style="border:1px solid #e0e0e0;padding:5px;"><strong>Customer:</strong></td><td style="border:1px solid #e0e0e0;padding:5px;">' . strip_tags($_POST['customer']) . '</td></tr>';
+$message .= '<tr style="background:rgba(0,0,0,0.05);line-height:175%;"><td style="border:1px solid #e0e0e0;padding:5px;"><strong>Comments:</strong></td><td style="border:1px solid #e0e0e0;padding:5px;">' . strip_tags($_POST['comments']) . '</td></tr>';
+$message .= '</table>';
+$message .= '</div>';
+$message .= '</body></html>';
+
+
 
 // To send HTML mail, the Content-type header must be set
 $headers[] = 'MIME-Version: 1.0';
 $headers[] = 'Content-type: text/html; charset=iso-8859-1';
 
 // Additional headers
-$headers[] = 'To: Wayne <example@yahoo.com>';
-$headers[] = 'From: example <example@yahoo.com>';
+// $headers[] = 'To: Wayne Hite <waynehite@yahoo.com>';
+$headers[] = 'From: Hite Autobody & Restoration Site Visitor <hiteauto@hiteautobodyandrestoration.com>';
+$headers[] = 'Reply-To: ' . $email;
    
-// $authorized = '-fexample@yahoo.com';
-// $result = mail($to, $subject, $message, $headers, $authorized);
-
- mail($to, $subject, $message, implode("\r\n", $headers));
+$result = mail($to, $subject, $message, implode("\r\n", $headers));
  
  
  
-echo $result ? '<h3>Hello, ' . $name . '.</h3>' . '<h4>Thank you for contacting example Autobody & Restoration!</h4>' . '<h4>We have received your message and will reply as soon as possible.</h4>' : '<h4>Sorry, your mail has not sent. Please try again.</h4>';
+echo $result ? '<h3>Hello, ' . $name . '.</h3>' . '<h4>Thank you for contacting Hite Autobody & Restoration!</h4>' . '<h4>We have received your message and will reply as soon as possible.</h4>' : '<h4>Sorry, your mail has not sent. Please try again.</h4>';
 
